fa57a997cf083e53e7318892c76c6a9f62a69462
GoldenArt-IT <store.goldenart@gmail.com>
2025-05-05 11:05:33 +0800

    feat : add show metric duplicated and show duplicated table in stock records

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from streamlit_gsheets import GSheetsConnection
 from core.data_reader import load_sheet_data, load_sheet_record
 from core.file_processor import process_uploaded_file, merge_and_label
 from core.filters import apply_filters
-from core.metrics import show_metrics
-from core.tables import show_tables
+from core.metrics import show_metrics, show_metric_for_duplicated_data
+from core.tables import show_tables, show_records
 import time
 
 def main():
@@ -14,9 +14,9 @@ def main():
     start = time.time()
     conn = st.connection("gsheets", type=GSheetsConnection)
     df_sheet = load_sheet_data(conn)
-    df_record = load_sheet_record(conn).tail(1)
+    df_record = load_sheet_record(conn)
     st.write(f"⏱ Load Sheet Time: {time.time() - start:.2f}s")
-    st.write(f"🆕Latest data key in GA Store: {df_record['TIMESTAMP'].values}")
+    st.write(f"🆕Latest data key in GA Store: {df_record.tail(1)['TIMESTAMP'].values}")
 
     uploaded_file = st.sidebar.file_uploader("Please upload file", type=["csv", "xlsx"])
     if uploaded_file:
@@ -26,8 +26,12 @@ def main():
 
         show_metrics(df_merged)
         show_tables(df_merged)
+        show_metric_for_duplicated_data(df_record)
+        show_records(df_record)
     else:
-        st.warning("Please upload file", icon="⚠")
+        st.warning("Please upload Autocount file", icon="⚠")
+        show_metric_for_duplicated_data(df_record)
+        show_records(df_record)
 
 if __name__ == "__main__":
     main()
